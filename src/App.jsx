98bc60f295f91523d5a6c72ece68dcf65a85e8af
Ganesh Kannan <ganeshkannan995gk@gmail.com>
2025-08-11 21:21:55 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+function App() {
+  return (
+    <>
+      <div className="container mt-5">
+        <h2 class="mb-4 text-center">Student Management System</h2>
+        <a href="add.php" class="btn btn-success mb-3">
+          + Add Student
+        </a>
+        <table class="table table-bordered table-striped">
+          <thead class="table-primary">
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Phone</th>
+              <th>Update</th>
+              <th>Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>01</td>
+              <td>John</td>
+              <td>john@gmail.com</td>
+              <td>9876543210</td>
+              <td><button className="btn btn-warning">Update</button></td>
+              <td><button className="btn btn-danger">Delete</button></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
+}
+
+export default App;
