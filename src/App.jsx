b7a9239564dc2bc4fba8ffb6815de4fd0a509c4c
Ganesh Kannan <ganeshkannan995gk@gmail.com>
2025-08-12 16:19:44 +0530

    Updated StudentList component

--- src/App.jsx
+++ src/App.jsx
@@ -15,34 +15,9 @@ function App() {
     <>
       <div className="container mt-5">
         <h2 class="mb-4 text-white text-center">Student Management System</h2>
-        <a href="add.php" class="btn btn-success mb-3">
+        <a href="#" class="btn btn-success mb-3">
           + Add Student
         </a>
-        <table class="table table-bordered table-striped">
-          <thead class="table-primary">
-            <tr>
-              <th>ID</th>
-              <th>Name</th>
-              <th>Email</th>
-              <th>Phone</th>
-              <th>Action</th>
-            </tr>
-          </thead>
-          <tbody>
-            {students.map(user =>
-            <tr key={user.id}>
-              <td>{user.id}</td>
-              <td>{user.name}</td>
-              <td>{user.email}</td>
-              <td>{user.phone}</td>
-              <td>
-                <button className="btn btn-warning ms-5">Update</button>
-                <button className="btn btn-danger ms-5">Delete</button>
-              </td>
-            </tr>
-            )}
-          </tbody>
-        </table>
       </div>
     </>
   );
---
+++ src/components/StudentList.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const StudentList = ({ students, onDelete }) => {
+  return (
+    <div>
+        <table class="table table-bordered table-striped mt-3">
+          <thead class="table-primary">
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Phone</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {students.map(user =>
+            <tr key={user.id}>
+              <td>{user.id}</td>
+              <td>{user.name}</td>
+              <td>{user.email}</td>
+              <td>{user.phone}</td>
+              <td>
+                <Link to={`/edit/${user.id}`} className="btn btn-warning me-2">Update</Link>
+                <button className="btn btn-danger" onClick={() => onDelete(user.id)}>Delete</button>
+              </td>
+            </tr>
+            )}
+          </tbody>
+        </table>
+    </div>
+  )
+}
+
+export default StudentList
