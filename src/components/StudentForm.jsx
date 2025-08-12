2dd1f12b39961b2bb018b7714a45c544911a3169
Ganesh Kannan <ganeshkannan995gk@gmail.com>
2025-08-12 19:14:39 +0530

    Updated StudentForm component

---
+++ src/components/StudentForm.jsx
@@ -0,0 +1,75 @@
+import React, { useState, useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+
+const StudentForm = ({ students = [], onSubmit }) => {
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  const existingStudent = students.find((s) => s.id === parseInt(id)) || {
+    name: "",
+    email: "",
+    phone: "",
+  };
+
+  const [formData, setFormData] = useState(existingStudent);
+
+  useEffect(() => {
+    setFormData(existingStudent);
+  }, [existingStudent]);
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault(); //prevents page refresh
+    onSubmit(formData); // tells App.jsx to save the data
+    navigate("/"); // go back to the list
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="mt-3">
+      <div className="mb-3">
+        <label>Name</label>
+        <input
+          type="text"
+          name="name"
+          className="form-control"
+          value={formData.name}
+          onChange={handleChange}
+          required
+        />
+      </div>
+
+      <div className="mt-3">
+        <label>Email</label>
+        <input
+          type="email"
+          name="email"
+          className="form-control"
+          value={formData.email}
+          onChange={handleChange}
+          required
+        />
+      </div>
+
+      <div className="mt-3">
+        <label>Phone</label>
+        <input
+          type="text"
+          name="text"
+          className="form-control"
+          value={formData.phone}
+          onChange={handleChange}
+          required
+        />
+      </div>
+
+      <button type="submit" className="btn btn-primary">
+        {id ? "Update" : "Add"} Student
+      </button>
+    </form>
+  );
+};
+
+export default StudentForm;
